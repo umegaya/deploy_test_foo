b224aa9aee6f8cda0088f3e38cc267935b63d80b
Takehiro Iyatomi <iyatomi@gmail.com>
2015-04-13 15:34:04 +0900

    update submodule and main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,15 @@
-local bar = require 'bar'
-local baz = require 'baz'
+local mod = require 'luact.module'
+local bar = mod.require 'bar'
+local baz = mod.require 'baz'
 
 assert(bar:fn() == 'foo.baz ver2')
 assert(baz:fn() == 'foo.baz')
 
-return true
+return {
+	bar = function ()
+		return bar:fn()
+	end,
+	baz = function ()
+		return baz:fn()
+	end,
+}
